52ee9fad1d2d04a4c6bd4381d70a21e3c6116e90
Jonathan Nwokeji <nwokejij@unc.edu>
2024-05-26 19:06:35 -0400

    pass stage

--- app/main.js
+++ app/main.js
@@ -3,7 +3,7 @@ const path = require("path");
 const zlib = require("zlib");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+// console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const command = process.argv[2];
