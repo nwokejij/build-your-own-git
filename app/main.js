5e55aa14e52a1a996421252a8f300e428c40f28a
Jonathan Nwokeji <nwokejij@unc.edu>
2024-05-27 19:21:01 -0400

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const zlib = require("zlib");
+const crypto = require('crypto');
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 // console.log("Logs from your program will appear here!");
@@ -16,6 +17,10 @@ switch (command) {
     const hash = process.argv[4];
     readBlobContent(hash);
     break;
+  case "hash-object":
+    const file = process.argv[4];
+    computeHash(file);
+    break;
   default:
     throw new Error(`Unknown command ${command}`);
 }
@@ -28,8 +33,31 @@ function createGitDirectory() {
   console.log("Initialized git directory");
 }
 async function readBlobContent(hash) {
-  const content = await fs.readFileSync(path.join(process.cwd(), ".git", "objects", hash.slice(0, 2), hash.slice(2)));
+  const content = await fs.readFileSync(path.join(process.cwd(), ".git", "objects", hash.slice(0, 2), hash.slice(2))); //reads blob content at 
 	const dataUnzipped = zlib.inflateSync(content); 
 	const res = dataUnzipped.toString().split('\0')[1]; //
 	process.stdout.write(res); // print result to stdout
 }
+async function computeHash(file){
+  // hash = file.slice(0,2) + file.slice(2);
+// header + size
+try{
+  const data = await fs.readFileSync(file, 'utf-8');
+  const size = data.length;
+  const header = `blob ${size}\0`;
+  const store = header + data;
+  const hash = crypto.createHash('sha-1').update(store).digest('hex');
+  try {
+    fs.writeFileSync(path.join(process.cwd(), ".git", "objects", hash.slice(0, 2), hash.slice(2)), file);
+    } catch (err) {
+      console.log("Error: Couldn't Write File");
+    }
+    process.stdout.write(hash);
+ } catch (err) {
+    console.log("Error: Couldn't Read File");
+  }
+  
+  
+
+}
+
