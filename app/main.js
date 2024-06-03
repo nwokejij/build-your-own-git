67c2d31ab6e0f1d5c9680922abe5b2b3767a654b
Jonathan Nwokeji <nwokejij@unc.edu>
2024-06-02 19:20:11 -0500

    pass stage

--- app/main.js
+++ app/main.js
@@ -191,18 +191,21 @@ function cloneRepo(url, dir){
   url += "/info/refs?service=git-upload-pack";
  
   https.get(url, (res) => {
-    response.on('error', (error) => {
+    if (res === undefined){
+      console.log("Error");
+    }
+    res.on('error', (error) => {
       console.error('An error occurred:', error);
     });
     let data = "";
     
   // As data comes in, append it to the 'data' variable
-  response.on('data', (chunk) => {
+  res.on('data', (chunk) => {
     data += chunk;
   });
   
   // When the response ends, log the data received
-  response.on('end', () => {
+  res.on('end', () => {
     console.log('Data received:', data);
     return data;
   });
